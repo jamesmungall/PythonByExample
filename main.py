313e9083d1325de39b439121bfb5be8a8288edf3
jamesmungall <peanuts@jamesmungall.co.uk>
2022-10-03 11:13:19 +0100

    Second commmit, Python by Example

--- main.py
+++ main.py
@@ -11,3 +11,10 @@
 # num2 = int(input("Enter another number "))
 # print("The sum of these number is", num1, " + ", num2, " = ", num1 + num2)
 
+# Challenge 005
+# num1 = int(input("Enter 1st number "))
+# num2 = int(input("Enter 2nd number "))
+# num3 = int(input("Enter 3rd number "))
+# print("(", num1, "+", num2, ")*", num3, "=", (num1+num2)*num3)
+
+
