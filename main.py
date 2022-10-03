01a4e1c0c27b416b39623c71ec0a70a4691d910a
jamesmungall <peanuts@jamesmungall.co.uk>
2022-10-03 11:08:10 +0100

    Second commmit, Python by Example

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-# This is a sample Python script.
+# Challenge 001, 002
+# firstname = input("Enter your first name: ")
+# surname = input("Enter your second name: ")
+# print("your name is ", firstname, surname)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Challenge 003
+# print("What do you call a bear with no teeth?\nA gummy bear!")
 
+# Challenge 004
+# num1 = int(input("Enter a number "))
+# num2 = int(input("Enter another number "))
+# print("The sum of these number is", num1, " + ", num2, " = ", num1 + num2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-    num1 = 93
